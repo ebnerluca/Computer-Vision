aa94efed238636d16d59191d8bf62612bae0f3be
ebnerluca <luca.ebnerr@hotmail.com>
2020-10-22 15:25:24 +0200

    Completed Ex1: Line Fitting

---
+++ Lab04 - Model Fitting/model_fitting/model_fitting/src/epipolar_geometry/distPointsLines.m
@@ -0,0 +1,10 @@
+% Compute the distance for pairs of points and lines
+% Input
+%   points    Homogeneous 2D points 3xN
+%   lines     2D homogeneous line equation 3xN
+% 
+% Output
+%   d         Distances from each point to the corresponding line N
+function d = distPointsLines(points, lines)
+end
+
---
+++ Lab04 - Model Fitting/model_fitting/model_fitting/src/epipolar_geometry/fundamentalMatrix.m
@@ -0,0 +1,10 @@
+% Compute the fundamental matrix using the eight point algorithm
+% Input
+% 	x1s, x2s 	Point correspondences 3xN
+%
+% Output
+% 	Fh 		Fundamental matrix with the det F = 0 constraint
+% 	F 		Initial fundamental matrix obtained from the eight point algorithm
+%
+function [Fh, F] = fundamentalMatrix(x1s, x2s)
+end
